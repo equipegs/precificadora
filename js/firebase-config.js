a33aa60d552f21e62997d6012c4df99cfda08442
equipegs <equipegs.contato@gmail.com>
2026-05-12 08:16:06 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,13 +5,13 @@
 // ══════════════════════════════════════════════════════════════
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyB4QTfZNC2tbSuv4vku91xIHb_UsWhTRoM",
-  authDomain:        "precifica-ecom.firebaseapp.com",
-  projectId:         "precifica-ecom",
-  storageBucket:     "precifica-ecom.firebasestorage.app",
-  messagingSenderId: "29019511944",
-  appId:             "1:290195119444:web:67bae42d8e8ae4fbd63cc"
-  measurementId:     "G-TVD9KDSSLX"
+  apiKey: "AIzaSyB4QTfZNC2tbSuv4vku91xIHb_UsWhTRoM",
+  authDomain: "precifica-ecom.firebaseapp.com",
+  projectId: "precifica-ecom",
+  storageBucket: "precifica-ecom.firebasestorage.app",
+  messagingSenderId: "290195119444",
+  appId: "1:290195119444:web:67bae42d8e8ae4fbd63cc1",
+  measurementId: "G-TVD9KDSSLX"
 };
 
 firebase.initializeApp(firebaseConfig);
